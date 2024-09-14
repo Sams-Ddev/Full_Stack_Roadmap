c75d51132fb966f1a3d1ba1e27c32c62d5efa31a
Sams-Ddev <sams.alxstudent@gmail.com>
2024-09-14 21:57:49 +0100

    oneliners

--- javascript/basic_concepts.js
+++ javascript/basic_concepts.js
@@ -17,8 +17,20 @@ var display = txt.length;
 window.alert(display);
 
 /**
- * Oneliners from the above
+ * Oneliners from the above: almost correct,
+ * but there's a small issue with the logic.
+ * If the user cancels the prompt or doesn't enter any text,
+ * prompt() will return null, which will cause an error when
+ * you try to access the length property of null.
  *
  */
 
 window.alert(prompt("Enter you text here").length);
+
+//Corected version of Oneliner.
+let userInput = prompt("Enter your text here");
+if (userInput !== null) {
+  window.alert(userInput.length);
+} else {
+  window.alert("No input provided.");
+}
