3d8d4782bc7a6a687b2533afa956756da895d024
wall7Ddev <moneywall7@gmail.com>
2025-04-09 19:59:37 +0000

    css positioning

---
+++ My_HTML/HTML_concepts/copy.js
@@ -0,0 +1,10 @@
+{
+  "Script": {
+    "Intro":"40-70 characters",
+    "Base":"280-350 characters"
+    "CTA": "55 characters"
+  },
+  "Title":"30-50 characters (Short and engaging), Keep it curiosity-driven (e.g., 'I tried THIS for 7 Days... Crazy Results!')",
+  "Desciption": "50-150 characters (Shorter, with key hashtags), hook,
+  Add AudioContext, CTA, and hashtags."
+}
